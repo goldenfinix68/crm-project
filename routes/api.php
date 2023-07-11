f0da00d57484dc9edaa0a70ae02d33b76e4279e4
Joshua Saubon <joshuasaubon@gmail.com>
2023-07-12 00:48:57 +0800

    fix

--- routes/api.php
+++ routes/api.php
@@ -22,24 +22,22 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::prefix('api')->group(function () {
-    Route::get('/telnyx/call/webhook', function(Request $request) {
-        \Log::error('INCOMING CALL SUCCESS');
-        \Log::error($request);
-    });
-    Route::get('/telnyx/call/webhook/fail', function(Request $request) {
-        \Log::error('INCOMING CALL FAIL');
-        \Log::error($request);
-    });
+Route::get('/telnyx/call/webhook', function(Request $request) {
+    \Log::error('INCOMING CALL SUCCESS');
+    \Log::error($request);
+});
+Route::get('/telnyx/call/webhook/fail', function(Request $request) {
+    \Log::error('INCOMING CALL FAIL');
+    \Log::error($request);
+});
 
-    Route::get('/telnyx/sms/webhook', function(Request $request) {
-        \Log::error('INCOMING SMS SUCCESS');
-        $json = json_decode(file_get_contents("php://input"), true);
-        \Log::error($json);
-    });
-    Route::get('/telnyx/sms/webhook/fail', function(Request $request) {
-        \Log::error('INCOMING SMS FAIL');
-        $json = json_decode(file_get_contents("php://input"), true);
-        \Log::error($json);
-    });
+Route::get('/telnyx/sms/webhook', function(Request $request) {
+    \Log::error('INCOMING SMS SUCCESS');
+    $json = json_decode(file_get_contents("php://input"), true);
+    \Log::error($json);
+});
+Route::get('/telnyx/sms/webhook/fail', function(Request $request) {
+    \Log::error('INCOMING SMS FAIL');
+    $json = json_decode(file_get_contents("php://input"), true);
+    \Log::error($json);
 });
