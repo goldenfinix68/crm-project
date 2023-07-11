69364366a67ff9a64faf49261942d5cbfb9f2ae3
Joshua Saubon <joshuasaubon@gmail.com>
2023-07-11 12:32:47 +0800

    071123_joshua_telnyx_webhook_routes

--- routes/api.php
+++ routes/api.php
@@ -19,3 +19,22 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
+Route::prefix('api')->group(function () {
+    Route::post('/telnyx/call/webhook', function(Request $request) {
+        \Log::error('INCOMING CALL SUCCESS');
+        \Log::error($request);
+    });
+    Route::post('/telnyx/call/webhook/fail', function(Request $request) {
+        \Log::error('INCOMING CALL FAIL');
+        \Log::error($request);
+    });
+
+    Route::post('/telnyx/sms/webhook', function(Request $request) {
+        \Log::error('INCOMING SMS SUCCESS');
+        \Log::error($request);
+    });
+    Route::post('/telnyx/sms/webhook/fail', function(Request $request) {
+        \Log::error('INCOMING SMS FAIL');
+        \Log::error($request);
+    });
+});
