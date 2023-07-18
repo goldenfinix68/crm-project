b5dcfe8ca13e185832a5646c466b25cb06de6319
Jepril Achapero <jeprilachapero@gmail.com>
2023-07-18 13:17:25 +0800

    update

--- resources/js/components/Navigation.tsx
+++ resources/js/components/Navigation.tsx
@@ -1,18 +1,104 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React, { useEffect, useState } from "react";
+import { Col, Dropdown, Input, Row, Space, Typography } from "antd";
+import type { MenuProps } from "antd";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCircle, faSearch } from "@fortawesome/free-solid-svg-icons";
 
-const Navigation = () => {
+import { Link } from "react-router-dom";
+import { LoadingOutlined } from "@ant-design/icons";
+
+const items: MenuProps["items"] = [
+    {
+        key: "1",
+        label: (
+            <div className="list-data">
+                <img src="https://files.salesmate.io/clinkup.salesmate.io/profilepic/9bfff1ff-8021-4f0b-a1c1-2c614882ac6b.jpg" />
+                <Space direction="vertical" size={0} className="m-l-xs">
+                    <Typography.Text>AL Sedevic Rome Twp Zonin</Typography.Text>
+                    <Typography.Text className="list-data-info">
+                        <Space
+                            split={
+                                <FontAwesomeIcon
+                                    icon={faCircle}
+                                    style={{
+                                        fontSize: 4,
+                                        verticalAlign: 3,
+                                    }}
+                                />
+                            }
+                        >
+                            <Typography.Text
+                                className="list-data-info"
+                                style={{
+                                    fontWeight: 300,
+                                }}
+                            >
+                                Mobile: +14405633236
+                            </Typography.Text>
+                            <Typography.Text
+                                className="list-data-info"
+                                style={{
+                                    fontWeight: 300,
+                                }}
+                            >
+                                Phone: (440) 487-8277
+                            </Typography.Text>
+                        </Space>
+                    </Typography.Text>
+                </Space>
+            </div>
+        ),
+    },
+];
+
+// const items: MenuProps["items"] = [
+//     {
+//         key: "1",
+//         label: (
+//             <>
+//                 <LoadingOutlined />
+//             </>
+//         ),
+//     },
+// ];
+
+const Navigation: React.FC = () => {
+    const [searchLoading, setSearchLoading] = useState(true);
+    useEffect(() => {
+        const timeoutId = setTimeout(() => {
+            setSearchLoading(false);
+        }, 1500);
+        return () => {
+            clearTimeout(timeoutId);
+        };
+    }, []);
+
     return (
-        <nav>
-            <ul>
-                <li>
-                    <Link to="/">Welcome</Link>
-                </li>
-                <li>
-                    <Link to="/home">Home</Link>
-                </li>
-            </ul>
-        </nav>
+        <>
+            <Row gutter={12}>
+                <Col span={12}>{/* Left Side  */}</Col>
+                <Col span={12}>
+                    {/* Right Side  */}
+                    <Row gutter={12} justify={"end"}>
+                        <Col span={10}>
+                            <Dropdown
+                                menu={{ items }}
+                                placement="bottomRight"
+                                arrow
+                                overlayClassName="header-search-bar"
+                                trigger={["click"]}
+                            >
+                                <Input
+                                    placeholder="Search"
+                                    prefix={<FontAwesomeIcon icon={faSearch} />}
+                                />
+                            </Dropdown>
+                        </Col>
+                        <Col span={10}></Col>
+                    </Row>
+                </Col>
+            </Row>
+        </>
     );
 };
 
--- resources/js/components/SideMenu.tsx
+++ resources/js/components/SideMenu.tsx
@@ -13,6 +13,7 @@ import {
 import { Layout, Menu, Button, theme } from "antd";
 import { useNavigate } from "react-router-dom";
 import Deal from "../pages/Deal";
+import Navigation from "./Navigation";
 
 const { Header, Sider, Content } = Layout;
 
@@ -76,9 +77,9 @@ const SideMenu = ({ children }) => {
                 />
             </Sider>
             <Layout>
-                <Header
-                    style={{ padding: 0, background: colorBgContainer }}
-                ></Header>
+                <Header style={{ padding: 0, background: colorBgContainer }}>
+                    <Navigation />
+                </Header>
                 <Content
                     style={{
                         padding: 24,
