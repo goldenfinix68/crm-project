2ad8a56358178237c86c8234f9eca875edb1fc3f
Jepril Achapero <jeprilachapero@gmail.com>
2023-08-24 14:10:30 +0800

    udpate

--- app/Models/Activity.php
+++ app/Models/Activity.php
@@ -4,13 +4,39 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Spatie\Activitylog\Traits\LogsActivity;
 
 class Activity extends Model
 {
-    use HasFactory;
+    use HasFactory, LogsActivity;
+
+    //history custom
+    protected static $logAttributes = [
+        "title",
+        "type",
+        "start_date",
+        "end_date",
+        "start_time",
+        "end_time",
+        "location",
+        "video_conferencing",
+        "recurrence",
+        "availability",
+        "internal_note",
+        "owner_id",
+        "deal_id",
+        "contact_id",
+        "status",
+        "completed_date",
+        "created_longitude",
+        "created_latitude",
+        "created_address",
+    ];
+    protected static $logName = "activity";
+    protected static $logOnlyDirty = true;
+    // protected static $recordEvents = ['deleted'];
 
     protected $guarded = [];
-
 
     public function activity_tags()
     {
@@ -31,4 +57,5 @@ class Activity extends Model
     {
         return $this->hasOne(\App\Models\User::class, 'id', 'owner_id');
     }
+
 }
