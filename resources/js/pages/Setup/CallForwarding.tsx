dedd28bb4ecae51b0cb8be44760e2e375400dba4
karlito0oo <73117222+karlito0oo@users.noreply.github.com>
2023-11-29 01:14:21 +0800

    refactor forwarding

--- resources/js/pages/Setup/CallForwarding.tsx
+++ resources/js/pages/Setup/CallForwarding.tsx
@@ -69,6 +69,7 @@ import { TUser } from "../../entities";
 import { Link } from "react-router-dom";
 import { sortUserCallForwardingMutation } from "../../api/mutation/useUserMutation";
 import { useAppContextProvider } from "../../context/AppContext";
+import DroppableList from "../../components/DroppableList";
 
 interface DragEndResult {
     source: DraggableLocation;
@@ -213,68 +214,3 @@ const CallForwarding: React.FC = ({}) => {
 };
 
 export default CallForwarding;
-
-interface DroppableListProps {
-    items?: TUser[];
-}
-const DroppableList: React.FC<DroppableListProps> = ({ items }) => (
-    <Droppable droppableId="list">
-        {(provided) => (
-            <div ref={provided.innerRef} {...provided.droppableProps}>
-                <List className="list-activity-type">
-                    {items?.map((item: any, index: number) => (
-                        <DraggableItem key={index} item={item} index={index} />
-                    ))}
-                    {provided.placeholder}
-                </List>
-            </div>
-        )}
-    </Droppable>
-);
-
-interface DraggableItemProps {
-    item: any;
-    index: number;
-}
-
-const DraggableItem: React.FC<DraggableItemProps> = ({ item, index }) => (
-    <Draggable draggableId={item.sortCallForwarding.toString()} index={index}>
-        {(provided) => (
-            <div
-                ref={provided.innerRef}
-                {...provided.draggableProps}
-                {...provided.dragHandleProps}
-            >
-                <List.Item className="list-item-activity-type">
-                    <Card
-                        className="list-item-card-activity-type"
-                        style={{
-                            display: "block",
-                            width: "100%",
-                        }}
-                    >
-                        <Row>
-                            <Col
-                                span={12}
-                                style={{
-                                    display: "flex",
-                                    alignItems: "center",
-                                }}
-                            >
-                                <HolderOutlined className="m-r-sm" />
-                                {item.firstName + " " + item.lastName}
-                            </Col>
-                            <Col span={12}>
-                                {item.numbers?.length
-                                    ? item.numbers
-                                          .map((number) => number.mobileNumber)
-                                          .join(", ")
-                                    : "Not Set"}
-                            </Col>
-                        </Row>
-                    </Card>
-                </List.Item>
-            </div>
-        )}
-    </Draggable>
-);
