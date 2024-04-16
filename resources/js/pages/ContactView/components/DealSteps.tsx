37a1e906a1ddd194715c916419ffd4387e838957
karlito0oo <73117222+karlito0oo@users.noreply.github.com>
2024-04-17 00:41:15 +0800

    [Contact View] Update deal by clicking stages timeline

--- resources/js/pages/ContactView/components/DealSteps.tsx
+++ resources/js/pages/ContactView/components/DealSteps.tsx
@@ -1,7 +1,10 @@
 import React from "react";
-import { Tooltip, Steps } from "antd";
+import { Tooltip, Steps, Popconfirm, message } from "antd";
 
 import { TDeal } from "../../../entities";
+import { useMutation } from "react-query";
+import { useDealMutation } from "../../../api/mutation/useDealMutation";
+import queryClient from "../../../queryClient";
 
 const { Step } = Steps;
 
@@ -15,12 +18,45 @@ const DealSteps = ({ deal }: Props) => {
         (stage) => stage.id === deal.stageId!
     );
 
+    const updateDeal = useMutation(useDealMutation, {
+        onSuccess: (res) => {
+            // navigate("/users"); // Redirect to the users list page after successful submission
+            if (res.success) {
+                message.success("Successfully Updated Deal");
+                queryClient.invalidateQueries("getContact");
+            } else {
+                message.error(res.message);
+            }
+        },
+        onError: (error: any) => {
+            message.error(error.message);
+        },
+    });
+
     return (
         <Steps size="small" current={currentStageIndex}>
             {stages?.map((stage, index) => (
                 <Step
                     key={stage.id}
-                    title={<Tooltip title={stage.name}>{stage.name}</Tooltip>}
+                    title={
+                        <Popconfirm
+                            title={`Do you want to change deal status to ${stage.name}?`}
+                            onConfirm={async () => {
+                                updateDeal.mutate({
+                                    ...deal,
+                                    stageId: stage.id,
+                                });
+                            }}
+                            icon={false}
+                        >
+                            <Tooltip
+                                title={stage.name}
+                                style={{ cursor: "pointer" }}
+                            >
+                                {stage.name}
+                            </Tooltip>
+                        </Popconfirm>
+                    }
                     style={{
                         opacity:
                             index < currentStageIndex!
@@ -28,6 +64,7 @@ const DealSteps = ({ deal }: Props) => {
                                 : index === currentStageIndex
                                 ? 1
                                 : 0.5,
+                        cursor: "pointer",
                     }}
                 />
             ))}
