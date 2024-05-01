be5e2285013e6652497bde1b42db81626588a7de
karlito0oo <73117222+karlito0oo@users.noreply.github.com>
2024-05-02 00:26:52 +0800

    [Contact Details] Lazy load

--- resources/js/pages/ContactView/index.tsx
+++ resources/js/pages/ContactView/index.tsx
@@ -1,5 +1,5 @@
 import { Col, Row, Space } from "antd";
-import React, { useEffect } from "react";
+import React, { Suspense, useEffect } from "react";
 import ContactInfo from "./components/ContactInfo";
 import ActionsTabs from "./components/ActionsTabs";
 import ContactsWall from "./components/ContactsWall";
@@ -36,6 +36,9 @@ const ContactView = () => {
     if (isLoading || !contact) {
         return <LoadingComponent />;
     }
+    const LazyContactsWall = React.lazy(
+        () => import("./components/ContactsWall")
+    );
 
     return (
         <ContactContext.Provider value={{ contact }}>
@@ -49,7 +52,9 @@ const ContactView = () => {
                     <Col span={10}>
                         {!isRoleStats && <ActionsTabs />}
                         <div style={{ paddingTop: "15px" }}>
-                            <ContactsWall />
+                            <Suspense fallback={<div>Loading...</div>}>
+                                <LazyContactsWall />
+                            </Suspense>
                         </div>
                     </Col>
                     <Col span={7}>
