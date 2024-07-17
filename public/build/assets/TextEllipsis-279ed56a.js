b59260664ac6410742e1f8891f873362893a90d2
karlito0oo <73117222+karlito0oo@users.noreply.github.com>
2024-07-18 01:54:56 +0800

    js build

--- public/build/assets/TextEllipsis-279ed56a.js
+++
@@ -1,5 +0,0 @@
-<<<<<<<< HEAD:public/build/assets/TextEllipsis-279ed56a.js
-import{J as s}from"./app-5c564086.js";const o=({children:e,style:t})=>{const l={...{whiteSpace:"nowrap",overflow:"hidden",textOverflow:"ellipsis",maxWidth:"calc(100% - 10px)",padding:0},...t};return s.createElement("div",{style:l},e)};export{o as T};
-========
-import{J as s}from"./app-5f014aea.js";const o=({children:e,style:t})=>{const l={...{whiteSpace:"nowrap",overflow:"hidden",textOverflow:"ellipsis",maxWidth:"calc(100% - 10px)",padding:0},...t};return s.createElement("div",{style:l},e)};export{o as T};
->>>>>>>> 7-15-24-Goran-kanban_board_pagination:public/build/assets/TextEllipsis-f00932cd.js
